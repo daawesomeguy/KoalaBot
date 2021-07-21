7ba9ec88c4cefb0bd7afeafc72a9afc365c90509
daawesomeguy <83517345+daawesomeguy@users.noreply.github.com>
2021-07-21 14:43:01 -0400

    Update pets.py

--- pets.py
+++ pets.py
@@ -1,8 +1,10 @@
 import requests,json,ijson
 q1=int(input('how many u want'))
+q2=int(input('how many whales'))
 r=requests.get('https://api.hypixel.net/skyblock/auctions',data={'auth':'378bce43-202d-469b-b357-e2cd995236b7','page':'0'})
 totalpages=r.json()['totalPages']
 katvalues=[]
+whalevalues=[]
 for i in range(1,totalpages):
     r=requests.get('https://api.hypixel.net/skyblock/auctions',data={'auth':'378bce43-202d-469b-b357-e2cd995236b7','page':i})
     x=r.json()
@@ -12,7 +14,16 @@ for i in range(1,totalpages):
                 katvalues.append(data['starting_bid'])
             else:
                 katvalues.append(data['highest_bid'])
+    for data in x['auctions']:
+        if 'Blue Whale' in data['item_name']:
+            if data['bin']==True:
+                #whalevalues[data['starting_bid']]=data['tier']
+                whalevalues.append([data['starting_bid'],data['tier'],data['item_name']])
+            else:
+                #whalevalues[data['highest_bid']]=data['tier']
+                whalevalues.append([data['highest_bid'],data['tier'],data['item_name']])
 z=katvalues.sort()
 for i in range(q1):
     print(katvalues[0])
     katvalues.pop(0)
+print(whalevalues)
