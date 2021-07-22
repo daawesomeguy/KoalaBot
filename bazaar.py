9d8299bf21fed7a407e145ed810eb281d5544c63
litblock <37122962+litblock@users.noreply.github.com>
2021-07-22 10:44:30 -0400

    Update bazaar.py

--- bazaar.py
+++ bazaar.py
@@ -3,8 +3,12 @@ r=requests.get('https://api.hypixel.net/skyblock/bazaar',data={'auth':'378bce43-
 x=r.json()['products']
 f = open("bazaar.txt", "a")
 for i in x:
-    f.write(x[i]['quick_status']['productId'] + ": ")
-    f.write(str(round(x[i]['quick_status']['buyPrice'] - x[i]['quick_status']['sellPrice'], 1)))
-    f.write("\n")
-    
-    
+    item = x[i]['quick_status']['productId'] + ": "
+    buy = round(x[i]['quick_status']['buyPrice'], 1)
+    sell = round(x[i]['quick_status']['sellPrice'], 1)
+    margin = round(buy - sell)
+    try:
+        percentage = round(margin/buy * 100, 1)
+        print(item + str(buy) + " " + str(sell) + " " + str(margin) + " " + str(percentage))
+    except:
+        print("error")
