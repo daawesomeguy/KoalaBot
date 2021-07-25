78cb9b7a1ac8f824a77305d9fbfb5ea353de0524
litblock <37122962+litblock@users.noreply.github.com>
2021-07-25 11:05:53 -0400

    Update bz.py

--- KoalaBot/cogs/bz.py
+++ KoalaBot/cogs/bz.py
@@ -13,11 +13,41 @@ class Bz(commands.Cog, name="Bazaar"):
 
     @commands.command()
     #async def petflip(self, ctx):
-    async def petflip(self, ctx):
+    async def bz(self, ctx):
         """
-        Bazaar flipper command
+        : Bazaar flipper command
         """
         await ctx.send("Getting profit...")
+        await ctx.send(bz())
         await ctx.send("Done!")
 def setup(client):
     client.add_cog(Bz(client))
+
+def bz():
+    r=requests.get('https://api.hypixel.net/skyblock/bazaar',data={'auth':'378bce43-202d-469b-b357-e2cd995236b7'})
+    x=r.json()['products']
+    f = open("bazaar.txt", "a")
+    items = []
+    for i in x:
+        item = x[i]['quick_status']['productId'] + ": "
+        buy = round(x[i]['quick_status']['buyPrice'], 1)
+        sell = round(x[i]['quick_status']['sellPrice'], 1)
+        margin = round(buy - sell)
+        try:
+            percentage = round(margin/buy * 100, 1)
+            items.append(item + str(buy) + " " + str(sell) + " " + str(margin) + " " + str(percentage))
+        except:
+            print("error")
+
+        #f.write(x[i]['quick_status']['productId'] + ": ")
+        #f.write(str(round(x[i]['quick_status']['buyPrice'] - x[i]['quick_status']['sellPrice'], 1)))
+        #f.write("\n")
+    return items
+    """
+    Weight system:
+
+    Buy volume
+    Sell volume
+    Margin
+    Percentage
+    """
