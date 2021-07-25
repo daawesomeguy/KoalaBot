63eb7b9d201cc47c4fca4761f8b01cf8602c02ec
daawesomeguy <83517345+daawesomeguy@users.noreply.github.com>
2021-07-25 11:38:24 -0400

    Update bz.py
    beta weight

--- KoalaBot/cogs/bz.py
+++ KoalaBot/cogs/bz.py
@@ -1,3 +1,4 @@
+
 import requests,json,ijson
 from operator import itemgetter
 from requests.models import Response
@@ -6,6 +7,7 @@ import numpy as np
 from discord.ext import commands
 import traceback
 import datetime
+from operator import itemgetter
 class Bz(commands.Cog, name="Bazaar"):
 
     def __init__(self, client):
@@ -24,10 +26,10 @@ def setup(client):
     client.add_cog(Bz(client))
 
 def bz():
+    items2=0
     try:
         r=requests.get('https://api.hypixel.net/skyblock/bazaar',data={'auth':'378bce43-202d-469b-b357-e2cd995236b7'})
         x=r.json()['products']
-        f = open("bazaar.txt", "a")
         items = []
         for i in x:
             item = x[i]['quick_status']['productId'] + ": "
@@ -35,14 +37,16 @@ def bz():
             sell = round(x[i]['quick_status']['sellPrice'], 1)
             margin = round(buy - sell)
             percentage = round(margin/buy * 100, 1)
-            items.append(item + str(buy) + " " + str(sell) + " " + str(margin) + " " + str(percentage))
-
-    except Exception as e:
-        print(e)
+            #items.append(item + str(buy) + " " + str(sell) + " " + str(margin) + " " + str(percentage))
+            weight=((buy+sell)/5+margin)*percentage
+            items.append([item,weight])
         #f.write(x[i]['quick_status']['productId'] + ": ")
         #f.write(str(round(x[i]['quick_status']['buyPrice'] - x[i]['quick_status']['sellPrice'], 1)))
         #f.write("\n")
-    return items
+    except Exception as e:
+        print(e)
+    items2=sorted(items, key=itemgetter(1),reverse=True)
+    return items2
     """
     Weight system:
     Buy volume
